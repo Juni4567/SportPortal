001462209f7bcdb3d343ba39e4a3dd7b9915d7fe
Junaid Anwar <junaid4567@gmail.com>
2015-09-02 12:53:36 +0500

    sp-nav fixed in responsive

--- layout/header.php
+++ layout/header.php
@@ -342,6 +342,12 @@
 
 		</header>
 		<div id="main">
-			<!--        --><?php
-//        echo $username;
-//    ?>
+            <?php if(isset($_SESSION['logged_user'])){ ?>
+                <ul class="nav nav-justified sp-left-bar-fixed">
+                    <li><a href="#"><i class="glyphicon glyphicon-cog"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-heart"></i></a></li>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-comment"></i><span class="count">3</span></a><ul class="dropdown-menu" role="menu"><li><a href="#">1. Is there a way..</a></li><li><a href="#">2. Hello, admin. I would..</a></li><li><a href="#"><strong>All messages</strong></a></li></ul></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-user"></i></a></li>
+                    <li><a title="Add Widget" data-toggle="modal" href="#addWidgetModal"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
+                </ul>
+                <?php } ?>
