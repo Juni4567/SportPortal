5bcd391ff0e3218646f8c62fc24662ef2b655322
md333 <md81332@gmail.com>
2015-09-22 15:54:45 +0500

    Players added to teams but some issues

--- teaminfo.php
+++ teaminfo.php
@@ -5,13 +5,24 @@ require('layout/header.php');
 <div class="container general-section"></div>
 <div id="score-board" class="general-section">
 	<div class="container">
-		<div class="row">
-      <div class="col-md-12">
         <center><h3>Team information</h3></center>
-        Raja g<br>
-        Pjipa
-      </div>
-    </div>
+        <?php
+       	  require_once 'includes/db_connect.php';
+       	  $query = "SELECT * FROM player WHERE g_id='1' AND dept_id='3' ";
+          $query_run = mysqli_query($mysqli, $query);
+       	  if(!$query_run->num_rows){
+        ?>
+          <h4 class="alert alert-danger">No matches are completed for this game right now</h4>
+          <?php
+            }
+            if ($query_run){
+              while ($query_row = mysqli_fetch_assoc($query_run)) {
+              $player_id = $query_row['player_id'];
+              $game_role = $query_row['game_role'];                    
+            ?>
+            <?php echo $player_id; ?><br>
+            <?php echo $game_role; ?><br>
+            <?php } }?>
 	</div>
 </div><!--score board end-->
 
