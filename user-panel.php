f38b3472e842cdc2071b7ba1ee8244a9c0cd3345
junaid Anwar <Junaid4567@gmail.com>
2015-09-23 16:19:13 +0500

    user panel

---
+++ user-panel.php
@@ -0,0 +1,36 @@
+<?php
+//include header template
+include('layout/header.php');
+?>
+<div class="general-section">
+    <div class="container">
+    <div class="sp-profile">
+        <div class="profile-header">
+            <div class="sp-userimg">
+                <img src="assets/images/user.png" alt="Junaid Anwar"/>
+            </div>
+            <div class="userinfo">
+                <h2>Junaid Anwar <span class="user-age">23</span> <span class="profile-status">***</span></h2>
+                <div class="userteam"><span class="teamname">BS(IT)</span></div>
+            </div>
+        </div>
+        <div class="profile-body">
+            <div class="profile-left">
+                <ul>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="#">Topics</a></li>
+                    <li><a href="#">Forum</a></li>
+                    <li><a href="#">Employers</a></li>
+                </ul>
+            </div>
+            <div class="profile-inner">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi, dicta dolorum eius eligendi eos excepturi exercitationem, facere fugiat ipsa laboriosam, modi necessitatibus nemo neque odit perspiciatis quam qui velit.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+<?php
+//include header template
+include('layout/footer.php');
+?>
