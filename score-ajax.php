f8afb4c095ff6b2580e875c3b79a42b2d0c704a6
junaid Anwar <Junaid4567@gmail.com>
2015-10-21 12:45:42 +0500

    score update function broken fixed

--- score-ajax.php
+++ score-ajax.php
@@ -1,44 +1,45 @@
-	<!--match container-->
+<div class="tab-content">
+    <!--match container-->
 
-	<?php
-	$tabs = array(
-		'cricket'   => 1,
-		'football'  => 2,
-		'hockey'    => 3,
-		'tennis'    => 4,
-		'vollyball' => 5
-	);
+    <?php
+    $tabs = array(
+        'cricket'   => 1,
+        'football'  => 2,
+        'hockey'    => 3,
+        'tennis'    => 4,
+        'vollyball' => 5
+    );
 
-	foreach ( $tabs as $tab_name => $tab_id ) :
+    foreach ( $tabs as $tab_name => $tab_id ) :
         ?>
-		<div role="tabpanel" class="tab-pane fade in <?php if($tab_id == 1){echo "active";} ?>" id="<?php echo $tab_name; ?>">
+        <div role="tabpanel" class="tab-pane fade in <?php if($tab_id == 1){echo "active";} ?>" id="<?php echo $tab_name; ?>">
 
 
-			<?php
-			require_once 'includes/db_connect.php';
+            <?php
+            require_once 'includes/db_connect.php';
 
-			$query     = "SELECT m.*, t1.team_name as `team_1_name`, t2.team_name as `team_2_name` FROM matches AS m INNER JOIN teams AS t1 ON (t1.team_id = m.team1_id) INNER JOIN teams AS t2 ON (t2.team_id = m.team2_id) WHERE m.matchstatus='live' AND m.g_id='$tab_id' ";
-			$query_run = mysqli_query( $mysqli, $query );
+            $query     = "SELECT m.*, t1.team_name as `team_1_name`, t2.team_name as `team_2_name` FROM matches AS m INNER JOIN teams AS t1 ON (t1.team_id = m.team1_id) INNER JOIN teams AS t2 ON (t2.team_id = m.team2_id) WHERE m.matchstatus='live' AND m.g_id='$tab_id' ";
+            $query_run = mysqli_query( $mysqli, $query );
 
-			if ( ! $query_run->num_rows ) {
-				?>
-				<h4 class="alert alert-danger">No matches are being played for this sport</h4>
-				<?php return;
-			}
+            if ( ! $query_run->num_rows ) {
+                ?>
+                <h4 class="alert alert-danger">No matches are being played for this sport</h4>
+                <?php return;
+            }
 
-			if ( $query_run ) {
-				?>
-				<div class="scorecard-header">
-					<h2>Live Now</h2>
-					<h4>Total <?php echo $query_run->num_rows; ?> <?php echo $tab_name; ?> matches are live now</h4>
-				</div>
-				<?php
-				while ( $query_row = mysqli_fetch_assoc( $query_run ) ) {
-					$team1_name        = $query_row['team_1_name'];
-					$team2_name        = $query_row['team_2_name'];
-					$match_date_time = $query_row['match_date_time'];
-					$location        = $query_row['location'];
-					$matchstatus     = $query_row['matchstatus'];
+            if ( $query_run ) {
+                ?>
+                <div class="scorecard-header">
+                    <h2>Live Now</h2>
+                    <h4>Total <?php echo $query_run->num_rows; ?> <?php echo $tab_name; ?> matches are live now</h4>
+                </div>
+                <?php
+                while ( $query_row = mysqli_fetch_assoc( $query_run ) ) {
+                    $team1_name        = $query_row['team_1_name'];
+                    $team2_name        = $query_row['team_2_name'];
+                    $match_date_time = $query_row['match_date_time'];
+                    $location        = $query_row['location'];
+                    $matchstatus     = $query_row['matchstatus'];
                     $match_id        = $query_row['match_id'];
                     $se_ls = "SELECT * from livescores where match_id='$match_id'";
                     $se_run = mysqli_query($mysqli, $se_ls);
@@ -46,31 +47,37 @@
                     $runs = $se_row['runs'];
                     $overs = $se_row['over'];
                     $wickets = $se_row['wicket'];
-                    // echo $team1_id. 'VS'.$team2_id. 'AT'. $match_date_time. 'LIVE FROM'.$location;
-					?>
-					<div class="scorecard-container">
-						<div class="match-card general-section">
-							<span class="live"><?php echo $matchstatus; ?></span>
-							<h4>At <?php echo $location; ?></h4>
+//             echo $team1_id. 'VS'.$team2_id. 'AT'. $match_date_time. 'LIVE FROM'.$location;
 
-							<div class="teams">
-								<h3><?php echo $team1_name; ?></h3>
+                    ?>
+                    <div class="scorecard-container">
+                        <div class="match-card general-section">
+                            <span class="live"><?php echo $matchstatus; ?></span>
+                            <h4>At <?php echo $location; ?></h4>
 
-								<h2>VS</h2>
+                            <div class="teams">
+                                <h3><?php echo $team1_name; ?></h3>
 
-								<h3><?php echo $team2_name; ?></h3>
-							</div>
+                                <h2>VS</h2>
+
+                                <h3><?php echo $team2_name; ?></h3>
+                            </div>
+                            <!--							<div class="match-time">-->
+                            <!--								<h4>Scheduled --><?php //echo $match_date_time; ?><!-- </h4>-->
+                            <!--							</div>-->
                             <div class="match-time">
                                 <h4><?php echo $runs.'/'.$wickets. '   Overs: '.$overs; ?> </h4>
                             </div>
-						</div>
-					</div>
-				<?php
-				}
-			} else {
-				echo "mysql_error()";
-			}
-			?>
+                        </div>
+                    </div>
+                <?php
+                }
+            } else {
+                echo "mysql_error()";
+            }
+            ?>
 
-		</div>
-	<?php endforeach; ?>
+        </div>
+    <?php endforeach; ?>
+
+</div>
--- scores.php
+++ scores.php
@@ -1,53 +1,55 @@
-<?php require( 'layout/header.php' ); ?>
-<div id="score-board" class="general-section">
-	<div class="container">
-		<div class="sport-nav tabbable">
-			<ul class="nav nav-tabs" role="tablist">
+<?php
+//include header template
+require( 'layout/header.php' );
+?>
 
-				<li role="presentation" class="active"><a href="#cricket" aria-controls="cricket" role="tab" data-toggle="tab">Cricket</a></li>
-				<li role="presentation"><a href="#football" aria-controls="football" role="tab" data-toggle="tab">Football</a></li>
-				<li role="presentation"><a href="#hockey" aria-controls="hockey" role="tab" data-toggle="tab">Hockey</a></li>
-				<li role="presentation"><a href="#tennis" aria-controls="tennis" role="tab" data-toggle="tab">Tennis</a></li>
-				<li role="presentation"><a href="#vollyball" aria-controls="vollyball" role="tab" data-toggle="tab">VollyBall</a></li>
-			</ul>
+<div id="score-board" class="general-section">
+    <div class="container">
+        <div class="sport-nav tabbable">
+            <ul class="nav nav-tabs" role="tablist">
+
+                <li role="presentation" class="active"><a href="#cricket" aria-controls="cricket" role="tab" data-toggle="tab">Cricket</a></li>
+                <li role="presentation"><a href="#football" aria-controls="football" role="tab" data-toggle="tab">Football</a></li>
+                <li role="presentation"><a href="#hockey" aria-controls="hockey" role="tab" data-toggle="tab">Hockey</a></li>
+                <li role="presentation"><a href="#tennis" aria-controls="tennis" role="tab" data-toggle="tab">Tennis</a></li>
+                <li role="presentation"><a href="#vollyball" aria-controls="vollyball" role="tab" data-toggle="tab">VollyBall</a></li>
+            </ul>
 
             <div id="ajax-score-card">
-                <div class="tab-content">
-                    <?php require_once ('score-ajax.php'); ?>
-                </div>
+                <?php require_once ('score-ajax.php'); ?>
             </div>
-		</div>
-    </div><!-- end tabbable	-->
+        </div>
+    </div>
+    <!-- end tabbable	-->
 </div><!--score board end-->
-</div><!--Added to manipulate footer extra margin comming from generalsection(closed general section before footer start) -->
+</div> <!--some tag that needs to be closed, taking footer into container if not closed-->
 <?php
 //include footer template
 require_once('layout/footer.php');
 ?>
 <script>
-	$(document).ready(function () {
-		$('.sp-nav').find('#scores').addClass('active').children('a').removeAttr('href');
+    $(document).ready(function () {
+        $('.sp-nav').find('#scores').addClass('active').children('a').removeAttr('href');
 
-		var score_card = $('#ajax-score-card');
+        var score_card = $('#ajax-score-card');
 
-		var refreshScores  = function() {
-			$.get('score-ajax.php', function(response) {
-				var response = $(response);
+        var refreshScores  = function() {
+            $.get('score-ajax.php', function(response) {
+                var response = $(response);
+                console.log('score updated');
+                response.find('.tab-pane').each(function(){
+                    score_card.find('#' + $(this).attr('id')).html($(this).html());
+                });
+            });
+        };
 
-				response.find('.tab-pane').each(function(){
-					score_card.find('#' + $(this).attr('id')).html($(this).html());
-				});
-			});
-		};
+        $('#ajax-score-card').on('click', '.sp-cta', function(e) {
+            e.preventDefault();
+//			alert('hello world');
+            return false;
+        });
 
-//        REmoved CTA
-//		$('#ajax-score-card').on('click', '.sp-cta', function(e) {
-//			e.preventDefault();
-////			alert('hello world');
-//			return false;
-//		});
-
-		//window.refreshScores = refreshScores;
-		setInterval(refreshScores, 5000);
-	});
+        //window.refreshScores = refreshScores;
+        setInterval(refreshScores, 5000);
+    });
 </script>
