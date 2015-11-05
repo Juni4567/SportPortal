dfe655d104599597b62a4d6d18f2c9407fc3e8c8
Mudassar Iqbal <md81332@gmail.com>
2015-11-05 10:39:55 +0500

    Adding timestamp autometically

--- admin/addnews.php
+++ admin/addnews.php
@@ -14,11 +14,8 @@ if (isset($_SESSION['logged_user']) && $_SESSION['user_role'] === 'Admin') {
 		                        <input type="text" autocomplete="off" name="newsheading" id="newsheading" required="required" class="text-center" placeholder="NEWS HEADING" class="form-control">
 		                    </div><br></br>
 		                    <div class="col-xs-12">
-		                        <input type="num-text" autocomplete="off" name="newsdes" id="newsdes" placeholder="NEWS DESCRIPTION" required="required" class="form-control">
+		                        <input type="num-text" autocomplete="off" name="newsdes" id="newsdes" placeholder="NEWS DESCRIPTION" required="required" class="form-control text-center">
 		                    </div><br></br>
-		                    <div class="input-center"class="col-xs-2">
-		                        Date:<input type="date" name="date" id="date" required="required">
-		                    </div>
 		                </div>
 				        <div>
 		                    <button type="submit" name="addnews" class="btn btn-info">Add news</button>
@@ -28,8 +25,8 @@ if (isset($_SESSION['logged_user']) && $_SESSION['user_role'] === 'Admin') {
 		                if (isset($_POST['addnews'])) {
 		                    $newsheading = $_POST["newsheading"];
 		                    $newsdescription = $_POST["newsdes"];
-		                    $date = $_POST["date"];
-		                    $query_insert = "INSERT INTO news (news_heading, newsdescription, date) VALUES('$newsheading', '$newsdescription', '$date')";
+		               //     $date = $_POST["date"];
+		                    $query_insert = "INSERT INTO news (news_heading, newsdescription, date) VALUES('$newsheading', '$newsdescription', now())";
 		                    $query_run_insert = mysqli_query($mysqli, $query_insert);
 		                }
 		                ?>
