91411398420ab626e1ffcb0d600611d3b9a44e16
junaid Anwar <Junaid4567@gmail.com>
2015-11-19 19:25:43 +0500

    Sub-coordinator now can create teams

--- score-ajax.php
+++ score-ajax.php
@@ -36,25 +36,32 @@
                 <?php
                 while ( $query_row = mysqli_fetch_assoc( $query_run ) ) {
                     $team1_name        = $query_row['team_1_name'];
+                    $team1_id          = $query_row['team1_id'];
                     $team2_name        = $query_row['team_2_name'];
-                    $match_date_time = $query_row['match_date_time'];
-                    $location        = $query_row['location'];
-                    $matchstatus     = $query_row['matchstatus'];
-                    $match_id        = $query_row['match_id'];
+                    $team2_id          = $query_row['team2_id'];
+                    $match_date_time   = $query_row['match_date_time'];
+                    $location          = $query_row['location'];
+                    $matchstatus       = $query_row['matchstatus'];
+                    $match_id          = $query_row['match_id'];
+
                 //    $query     = "SELECT m.*, t.team_name as `team_name` FROM livescores AS m INNER JOIN teams AS t1 ON (t.team_id = m.teaminnings) WHERE m.teaminnings='$teaminnings'";
                 //    $query_run = mysqli_query( $mysqli, $query );
-                    $se_ls = "SELECT l.*, t.team_name as `teaminnings` FROM livescores AS l INNER JOIN teams AS t ON (t.team_id = l.teaminnings) where match_id='$match_id'";
-                    $se_run = mysqli_query($mysqli, $se_ls);
-                    $se_row = mysqli_fetch_assoc($se_run);
-                    $runs = $se_row['runs'];
-                    $overs = $se_row['over'];
-                    $wickets = $se_row['wicket'];
+                    $se_ls      = "SELECT l.*, t.team_name as `teaminnings` FROM livescores AS l INNER JOIN teams AS t ON (t.team_id = l.teaminnings) where match_id='$match_id'";
+                    $se_run     = mysqli_query($mysqli, $se_ls);
+                    $se_row     = mysqli_fetch_assoc($se_run);
+                    $runs       = $se_row['runs'];
+                    $overs      = $se_row['over'];
+                    $wickets    = $se_row['wicket'];
                     $teaminnings = $se_row['teaminnings'];
 
                     // select Sum of scores
-                    $sum_scores = "SELECT SUM(runs), COUNT(over), SUM(wicket) , teaminnings FROM livescores where match_id='$match_id'";
-                    $query_run_scores = mysqli_query($mysqli, $sum_scores);
-                    $query_row_scores = mysqli_fetch_assoc($query_run_scores);
+                    $sum_scores1         = "SELECT SUM(runs), COUNT(over), SUM(wicket) , teaminnings FROM livescores where match_id='$match_id' AND teaminnings='$team1_id'";
+                    $query_run_scores1   = mysqli_query($mysqli, $sum_scores);
+                    $query_row_scores1   = mysqli_fetch_assoc($query_run_scores);
+
+                    $sum_scores2         = "SELECT SUM(runs), COUNT(over), SUM(wicket) , teaminnings FROM livescores where match_id='$match_id' AND teaminnings='$team2_id'";
+                    $query_run_scores2   = mysqli_query($mysqli, $sum_scores);
+                    $query_row_scores2   = mysqli_fetch_assoc($query_run_scores);
 
 
 //             echo $team1_id. 'VS'.$team2_id. 'AT'. $match_date_time. 'LIVE FROM'.$location;
@@ -77,9 +84,15 @@
                             <!--							</div>-->
                             <div class="match-time">
                                 <?php
+                                $team_se_inn = $query_row_scores1['teaminnings'];
+                                ?>
+                                <h4><?php echo $teaminnings;?> <?php echo $query_row_scores1['SUM(runs)'].'/'.$query_row_scores1['SUM(wicket)']. '   Overs: '.$query_row_scores1['COUNT(over)']; ?> </h4>
+                            </div>
+                            <div class="match-time">
+                                <?php
                                 $team_se_inn = $query_row_scores['teaminnings'];
                                 ?>
-                                <h4><?php echo $teaminnings;?> <?php echo $query_row_scores['SUM(runs)'].'/'.$query_row_scores['SUM(wicket)']. '   Overs: '.$query_row_scores['COUNT(over)']; ?> </h4>
+                                <h4><?php echo $teaminnings;?> <?php echo $query_row_scores2['SUM(runs)'].'/'.$query_row_scores2['SUM(wicket)']. '   Overs: '.$query_row_scores2['COUNT(over)']; ?> </h4>
                             </div>
                         </div>
                     </div>
--- uploadteamlogo.php
+++ uploadteamlogo.php
@@ -1,4 +1,33 @@
 <?php include('layout/header.php'); ?>
+
+
+<?php
+if (isset($_POST['addteam'])) {
+    //Add teamname, dept_id, g_id, sc_id and teamlogo to teams table
+    $teamname   = $_POST["teamname"];
+    $game_id    = $_POST["game"];
+    $dept_id    = $_GET["dept_id"];
+    $sc_id      = $_GET['sc_id'];
+    //check if the team already exists
+    $query_check_team       =  "SELECT * FROM teams WHERE team_name='$teamname'";
+    $query_run_check_team   = mysqli_query($mysqli, $query_check_team);
+    if(!mysqli_num_rows($query_run_check_team)){
+        echo" ". $teamname ." Team Inserted Successfully";
+        $query_insert = "INSERT INTO teams (team_name, dept_id, g_id, sc_id) VALUES('$teamname', '$dept_id', '$game_id' , '$sc_id')";
+        $query_run_insert = mysqli_query($mysqli, $query_insert);
+        if($query_run_insert){
+            echo "success";
+        }
+        else{
+            echo"failed";
+        }
+    }
+    else{
+        echo"". $teamname ." already Exists";exit;
+    }
+
+}
+?>
 <div class="container">
     <?php
     $logged_user = $_SESSION['logged_user'];
@@ -8,7 +37,7 @@
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
     // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
+    if (isset($_POST["addteam"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if ($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
@@ -28,14 +57,12 @@
         // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "uploads/teamlogos/" . $target_file)) {
-            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
             //Upload link to current user's row
             require_once 'includes/db_connect.php';
-            $query = "UPDATE teams set teamlogo='$target_file' LIMIT '1'";
-            if (mysqli_query($mysqli, $query)) {
-                ?>
+            $query = "UPDATE teams set teamlogo='$target_file' WHERE dept_id = '$dept_id' AND g_id='$game_id' AND sc_id='$sc_id'";
+            if (mysqli_query($mysqli, $query)) { ?>
     <script>
-        window.location.replace("user.php");
+        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
     </script>
     <?php
             }
